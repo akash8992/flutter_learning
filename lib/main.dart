4427e867617c7d71d10d2ee7f26348ba3c1e9add
akash <akash8992@gmail.com>
2022-08-08 15:29:51 +0530

    text positions

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,35 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Center(
-    child: Text(
-    'flutter first app',
+  runApp(Row(
     textDirection: TextDirection.ltr,
-    style: TextStyle(fontSize: 30.0),
-  )));
+    children: [
+      Expanded(
+        child:Text(
+        'one',
+        textDirection: TextDirection.ltr,
+        style: TextStyle(fontSize: 30),
+      )),
+      Expanded(
+        child:Text(
+        'Two',
+        textDirection: TextDirection.ltr,
+        style: TextStyle(fontSize: 30),
+      )),
+      Expanded(
+        child:Text(
+        'Three',
+        textDirection: TextDirection.ltr,
+        style: TextStyle(fontSize: 30),
+      )),
+     Expanded(
+        child:Text(
+        'Four',
+        textDirection: TextDirection.ltr,
+        style: TextStyle(fontSize: 30),
+      )),
+    ],
+  ));
 }
