fee1b694b327b53e81c9d0713a6183b8199c248a
akash <akash8992@gmail.com>
2022-08-09 22:12:53 +0530

    text positions

--- lib/main.dart
+++ lib/main.dart
@@ -3,33 +3,65 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Row(
-    textDirection: TextDirection.ltr,
-    children: [
-      Expanded(
-        child:Text(
-        'one',
-        textDirection: TextDirection.ltr,
-        style: TextStyle(fontSize: 30),
-      )),
-      Expanded(
-        child:Text(
-        'Two',
-        textDirection: TextDirection.ltr,
-        style: TextStyle(fontSize: 30),
-      )),
-      Expanded(
-        child:Text(
-        'Three',
-        textDirection: TextDirection.ltr,
-        style: TextStyle(fontSize: 30),
-      )),
-     Expanded(
-        child:Text(
-        'Four',
-        textDirection: TextDirection.ltr,
-        style: TextStyle(fontSize: 30),
-      )),
-    ],
-  ));
+  // runApp(Row(
+  //   textDirection: TextDirection.ltr,
+  //   children: [
+    //   Expanded(
+    //     child:Text(
+    //     'one',
+    //     textDirection: TextDirection.ltr,
+    //     style: TextStyle(fontSize: 30),
+    //   )),
+    //   Expanded(
+    //     child:Text(
+    //     'Two',
+    //     textDirection: TextDirection.ltr,
+    //     style: TextStyle(fontSize: 30),
+    //   )),
+    //   Expanded(
+    //     child:Text(
+    //     'Three',
+    //     textDirection: TextDirection.ltr,
+    //     style: TextStyle(fontSize: 30),
+    //   )),
+    //  Expanded(
+    //     child:Text(
+    //     'Four',
+    //     textDirection: TextDirection.ltr,
+    //     style: TextStyle(fontSize: 30),
+    //   )),
+  //   ],
+  // ));
+  // 
+  // runApp(Container( 
+  //   padding: EdgeInsets.symmetric(vertical: 50.0, horizontal: 40),
+  //   //padding: EdgeInsets.all(50.0),
+  //     decoration: BoxDecoration(color: Colors.blueAccent),
+  //   child: Column(
+  //     // ignore: prefer_const_literals_to_create_immutables
+  //     children: [
+  //        Text(
+  //         "1",
+  //         textDirection: TextDirection.ltr,
+  //         style:TextStyle(fontSize: 100),
+  //       ),  
+  //       Text(
+  //         "2",
+  //         textDirection: TextDirection.ltr,
+  //         style:TextStyle(fontSize: 100),
+  //       ), 
+  //       Text(
+  //         "3",
+  //         textDirection: TextDirection.ltr,
+  //         style:TextStyle(fontSize: 100),
+  //       ), 
+  //       Text(
+  //         "4",
+  //         textDirection: TextDirection.ltr,
+  //         style:TextStyle(fontSize: 100),
+  //       ), 
+  //     ],
+  //   ),
+  // ));
+  
 }
